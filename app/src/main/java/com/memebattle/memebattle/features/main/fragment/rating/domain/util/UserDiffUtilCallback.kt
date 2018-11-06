c4cc999857fd0d1deca80b11a0ae22a5ae6edfdb
seva <mrswimmerlab@gmail.com>
2018-11-06 16:10:44 +0700

    some fix

--- app/src/main/java/com/memebattle/memebattle/features/main/fragment/rating/domain/util/UserDiffUtilCallback.kt
+++ app/src/main/java/com/memebattle/memebattle/features/main/fragment/rating/domain/util/UserDiffUtilCallback.kt
@@ -1,7 +1,7 @@
 package com.memebattle.memebattle.features.main.fragment.rating.domain.util
 
 import androidx.recyclerview.widget.DiffUtil
-import com.memebattle.memebattle.features.main.fragment.rating.domain.model.recycler.User
+import com.memebattle.memebattle.features.main.fragment.rating.domain.model.res.User
 
 class UserDiffUtilCallback : DiffUtil.ItemCallback<User>() {
     override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
